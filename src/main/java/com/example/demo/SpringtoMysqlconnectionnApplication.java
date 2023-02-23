96de23729fd2ffb10ce7d54625b97d259450cda5
collegestuden <abinaya123567@gmail.com>
2023-02-23 15:20:09 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringtoMysqlconnectionnApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtoMysqlconnectionnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtoMysqlconnectionnApplication.class, args);
+	}
+
+}
